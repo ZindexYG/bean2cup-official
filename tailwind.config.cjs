6ed612276f9e2940daea4d4026db7e3aa465b461
z-index <yg_zinger@163.com>
2025-06-04 12:19:28 +0800

    feat: 更新配色相关

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,9 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#3986E2", // 添加主色为自定义颜色
+      },
       fontFamily: {
         sans: [
           "Bricolage Grotesque Variable",
